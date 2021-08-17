62b4a0e2890a4fc6748fa860078b28c67b56b2ca
Vishal <s.i.n.g.l.a.v.i.s.h.a.l.s.v@gmail.com>
2021-08-17 20:57:39 +0530

    Major changes

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var inputText=document.querySelector("#txt-input");
+var btnTranslate = document.querySelector("#btn");
+var outputDiv = document.querySelector("#output-txt");
+
+btnTranslate.addEventListener("click",clickHandler);
+
+function clickHandler { 
+    
+}
