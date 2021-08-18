cff152749dbd4b7f18d449ec0d7d25e51b92fd09
Vishal <s.i.n.g.l.a.v.i.s.h.a.l.s.v@gmail.com>
2021-08-18 11:31:49 +0530

    Added Translated text code

--- js/app.js
+++ js/app.js
@@ -2,8 +2,29 @@ var inputText=document.querySelector("#txt-input");
 var btnTranslate = document.querySelector("#btn");
 var outputDiv = document.querySelector("#output-txt");
 
-btnTranslate.addEventListener("click",clickHandler);
+var serverURL = ""
 
-function clickHandler { 
-    
+function getTranlationURL(input) {
+    return serverURL + "?" + "text=" + input
+
 }
+
+function errorHandler(error){
+console.log("Error Ocuured", error);
+alert("Something went wrong plaese try again later")
+}
+
+function clickHandler() { 
+var txtInput= inputText.value;
+
+fetch(getTranslationURL(txtInput))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.tranlated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler)
+
+};
+
+btnTranslate.addEventListener("click",clickHandler);
